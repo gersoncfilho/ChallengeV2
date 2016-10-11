313e690488821c3cf8b27e6138bb0e01b4891bc0
Gerson Cardoso Filho <gersoncfilho@mac.com>
2016-10-10 23:01:22 -0300

    Criacao das classes FragmentOpcoesAdapter e FragmentOpcoes

---
+++ app/src/main/java/gersondeveloper/com/br/challengev2/Adapter/FragmentOpcoesAdapter.java
@@ -0,0 +1,14 @@
+package gersondeveloper.com.br.challengev2.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by gerso on 10/10/2016.
+ */
+
+public class FragmentOpcoesAdapter extends RecyclerView<FragmentOpcoesAdapter.MyViewHolder> {
+
+
+
+
+}
---
+++ app/src/main/java/gersondeveloper/com/br/challengev2/Fragment/FragmentOpcoes.java
@@ -0,0 +1,12 @@
+package gersondeveloper.com.br.challengev2.Fragment;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by gerso on 10/10/2016.
+ */
+
+public class FragmentOpcoes extends Fragment implements View.OnClickListener {
+
+}
